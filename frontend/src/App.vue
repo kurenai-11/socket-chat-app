54e12c7cbdd26896e622209919f7dec813ada5bd
kurenai-11 <68825567+kurenai-11@users.noreply.github.com>
2023-01-03 14:23:50 +0300

    changes

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
+
+const navLinkClasses =
+  "h-full p-4 rounded-lg bg-zinc-7 decoration-none color-inherit font-bold transition-all hover:(text-zinc-1 bg-zinc-9)";
 </script>
 
 <template>
   <header>
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
+    <nav class="flex gap-2 py-2 px-1">
+      <RouterLink :class="navLinkClasses" to="/">Home</RouterLink>
+      <RouterLink :class="navLinkClasses" to="/about">About</RouterLink>
     </nav>
   </header>
 
