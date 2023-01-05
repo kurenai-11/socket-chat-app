1f92ed663812892be806a8f4e96b0eeb80765eb6
kurenai-11 <68825567+kurenai-11@users.noreply.github.com>
2023-01-05 12:29:07 +0300

    wording changes

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,10 +1,10 @@
 <template>
   <main class="flex flex-col items-center mt-6 gap-6">
     <h1 class="text-2xl text-center font-bold text-zinc-4">
-      This is a socket chat application made in Vue, using socket.io for real
-      time communication.
+      This is a socket chat application made in Vue(frontend) and
+      NodeJS(backend), using socket.io for real time communication.
     </h1>
-    <h2 class="text-lg text-zinc-3">
+    <h3 class="text-lg text-zinc-3">
       Made by
       <a
         href="https://github.com/kurenai-11"
@@ -12,6 +12,6 @@
         >kurenai-11</a
       >
       for web dev practice purposes.
-    </h2>
+    </h3>
   </main>
 </template>
