b0a4b99bdef633f6fdabfc8d1389de97aad8c1d2
kurenai-11 <68825567+kurenai-11@users.noreply.github.com>
2023-01-05 12:49:22 +0300

    reversed the colors here

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,10 +1,10 @@
 <template>
   <main class="flex flex-col items-center mt-6 gap-6">
-    <h1 class="text-2xl text-center font-bold text-zinc-4">
+    <h1 class="text-2xl text-center font-bold text-zinc-3">
       This is a socket chat application made in Vue(frontend) and
       NodeJS(backend), using socket.io for real time communication.
     </h1>
-    <h3 class="text-lg text-zinc-3">
+    <h3 class="text-lg text-zinc-4">
       Made by
       <a
         href="https://github.com/kurenai-11"
