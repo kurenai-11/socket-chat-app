c72d11a13a2617becbfc939d7aa148426d38d04e
kurenai-11 <68825567+kurenai-11@users.noreply.github.com>
2023-01-05 12:27:40 +0300

    other styling changes

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,9 +1,17 @@
 <template>
-  <main class="flex flex-col items-center mt-2 gap-2">
-    <h1 class="text-xl text-center font-bold text-zinc-4">
+  <main class="flex flex-col items-center mt-6 gap-6">
+    <h1 class="text-2xl text-center font-bold text-zinc-4">
       This is a socket chat application made in Vue, using socket.io for real
       time communication.
     </h1>
-    <h2 class="text-lg text-zinc-3"></h2>
+    <h2 class="text-lg text-zinc-3">
+      Made by
+      <a
+        href="https://github.com/kurenai-11"
+        class="text-red-6 decoration-none transition-all hover:(text-red-4)"
+        >kurenai-11</a
+      >
+      for web dev practice purposes.
+    </h2>
   </main>
 </template>
