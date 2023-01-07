8ef9adf183f0207da6eb2f8d343990a1f9462dfc
kurenai-11 <68825567+kurenai-11@users.noreply.github.com>
2023-01-07 19:21:20 +0300

    Delete backend/prisma/migrations directory

--- backend/prisma/migrations/20230107060244_add_u/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropIndex
-DROP INDEX "Message_createdAt_idx";
-
--- AlterTable
-ALTER TABLE "Message" ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
-
--- CreateIndex
-CREATE INDEX "Message_createdAt_updatedAt_idx" ON "Message"("createdAt", "updatedAt");
--- backend/prisma/migrations/20230107070358_add_roles/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "Role" AS ENUM ('User', 'Moderator', 'Admin');
-
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "role" "Role" NOT NULL DEFAULT 'User';
